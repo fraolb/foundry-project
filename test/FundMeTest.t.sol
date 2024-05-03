24874e533fbee2b019eb4bbe447b3471891dcd81
fraolb <fraolbereket@gmail.com>
2024-05-03 12:35:50 +0300

    testing contracts using foundry tools

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+    }
+
+    function testMinimumUSDIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsSender() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
+}
