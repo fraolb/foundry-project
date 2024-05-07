79886253ed9d973abda51c22c725eb7812b91cfb
fraolb <fraolbereket@gmail.com>
2024-05-07 14:15:56 +0300

    helper config and mock price feed

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.8.0;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
         vm.startBroadcast();
-        FundMe fundeMe = new FundMe();
+        FundMe fundeMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundeMe;
     }
